fcfe3c151191b9fb664e6006d15da39b9a6ad4bb
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-01-22 15:34:04 -0300

    Configuring TypeORM

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { response } from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.get('/', (request, response) =>
+    response.json({ message: 'Hello GoStack' }),
+);
+
+app.listen(3333, () => {
+    console.log('Server started on port 3333!');
+});
