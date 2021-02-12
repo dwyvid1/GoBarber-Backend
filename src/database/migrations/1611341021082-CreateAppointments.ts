b5e08b86c0a7d12850aa78b75541cafda0d45af7
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-02-11 21:08:48 -0300

    Trying to resolve an error

--- src/database/migrations/1611341021082-CreateAppointments.ts
+++ src/database/migrations/1611341021082-CreateAppointments.ts
@@ -20,7 +20,7 @@ export default class CreateAppointments1611341021082
                         isNullable: false,
                     },
                     {
-                        name: 'data',
+                        name: 'date',
                         type: 'timestamp with time zone',
                         isNullable: false,
                     },
