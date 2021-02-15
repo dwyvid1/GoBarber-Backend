7f7c99adddb4d9f5efa4494d422ae0cc66e09b08
Deyvid Nascimento <deyvid.nascimento0101@gmail.com>
2021-02-15 00:09:42 -0300

    Creating error class

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -14,11 +14,18 @@ sessionsRouter.post('/', async (request, response) => {
             password,
         });
 
-        delete user.password;
+        // delete user.password;
+        const userWithoutPassword = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        };
 
-        return response.json({ user, token });
+        return response.json({ userWithoutPassword, token });
     } catch (err) {
-        return response.status(400).json({ error: err.message });
+        return response.status(err.statusCode).json({ error: err.message });
     }
 });
 export default sessionsRouter;
